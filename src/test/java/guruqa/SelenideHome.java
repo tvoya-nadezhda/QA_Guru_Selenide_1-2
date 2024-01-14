19d3b23737d9356ca64130d52387abc0bab8f798
Tvoya.Nadezhda <tvoya.nadezhd@yandex.ru>
2024-01-15 01:38:45 +0300

    comm3

--- src/test/java/guruqa/SelenideHome.java
+++ src/test/java/guruqa/SelenideHome.java
@@ -1,32 +1,43 @@
 package guruqa;
 
 import com.codeborne.selenide.Configuration;
-import static com.codeborne.selenide.Selenide.*;
-
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class selenide_1 {
+public class SelenideHome {
     @BeforeAll
     static void beforeAll() {
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://github.com/";
+       // Configuration.holdBrowserOpen = true;
+       // Configuration.baseUrl = "";
     };
 @Test
     void selenideHomeTest(){
 
     // Откройте страницу Selenide в Github
-    open("selenide/selenide");
-    // Перейдите в раздел Wiki проекта
-
+    open("https://github.com/selenide/selenide");
+        // Перейдите в раздел Wiki проекта
+    $("#wiki-tab").click();
+    $("#wiki-pages-box > div > div.js-wiki-sidebar-toggle-display > ul > li.Box-row.wiki-more-pages-link > button").click();
     // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+    $("#wiki-pages-box").$(byText("SoftAssertions")).click();
     //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-
-   //$();
-
+    $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}")
+        );
     }
-
 }
 
